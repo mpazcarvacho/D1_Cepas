7fd1b75478389f101e5781962857a1e75e30b797
mariapaz <mpazcarvacho@gmail.com>
2021-11-19 19:12:18 -0300

    bootstrap and pages controller added

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+
+  def index
+    @wines = Wine.includes(:wines_strains).all
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :strains
   resources :wines
 
-  root to: 'wines#index'
+  root to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
